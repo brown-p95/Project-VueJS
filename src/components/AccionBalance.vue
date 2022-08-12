3edb2ea591d723a127f6789eab24e085917956b0
Pablo <jpgm.nomercy@gmail.com>
2022-08-12 11:52:48 -0500

    feat: :sparkles: Seccion5 complete

---
+++ src/components/AccionBalance.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1><button @click="accion" :disabled="disabled">{{text}}</button></h1>
+</template>
+
+<script>
+
+export default {
+    props: {
+        text: String,
+        disabled:{
+            type:Boolean,
+            default: false
+        } 
+    },
+    methods: {
+        accion(){
+            this.$emit('accion')
+        }
+    },
+
+}
+</script>
+
+<style scoped>
+ h1{
+    background-color: transparent;
+ }
+</style>
